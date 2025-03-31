31437ec3d7bb64eb5ac291a483dc19009b34dfcf
Bùi Minh Duy <duybui71103@gmail.com>
2025-03-31 21:16:23 +0700

    feat: add user entity

---
+++ src/main/java/com/hcmus/ecommerce_backend/user/model/entity/User.java
@@ -0,0 +1,62 @@
+package com.hcmus.ecommerce_backend.user.model.entity;
+
+import com.hcmus.ecommerce_backend.common.model.entity.BaseEntity;
+import com.hcmus.ecommerce_backend.user.model.enums.Role;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_num", nullable = false, unique = true)
+    private String phoneNum;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "weight")
+    private int weight;
+
+    @Column(name = "height")
+    private int height;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/hcmus/ecommerce_backend/user/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.hcmus.ecommerce_backend.user.model.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
