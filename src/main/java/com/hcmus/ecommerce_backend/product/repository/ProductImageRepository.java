f9223b1fff71de62b388a0712b411a284b3d0ea6
Bùi Minh Duy <duybui71103@gmail.com>
2025-04-17 17:27:03 +0700

    feat: add api get product images by id product and adjust 1 product has max 3 product_images

--- src/main/java/com/hcmus/ecommerce_backend/product/repository/ProductImageRepository.java
+++ src/main/java/com/hcmus/ecommerce_backend/product/repository/ProductImageRepository.java
@@ -1,5 +1,7 @@
 package com.hcmus.ecommerce_backend.product.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.hcmus.ecommerce_backend.product.model.entity.ProductImage;
@@ -7,4 +9,6 @@ import com.hcmus.ecommerce_backend.product.model.entity.ProductImage;
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, String> {
     boolean existsByUrlAndProductId(String url, String productId);
+    long countByProductId(String productId);
+    List<ProductImage> findByProductId(String productId);
 }
