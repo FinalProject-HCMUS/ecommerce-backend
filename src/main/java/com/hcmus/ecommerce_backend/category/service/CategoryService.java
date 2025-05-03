c0cc535337fea8cefdf52ffec7a420b7f6d93734
Bùi Minh Duy <duybui71103@gmail.com>
2025-05-03 15:39:42 +0700

    feat: get all categories without pagination

--- src/main/java/com/hcmus/ecommerce_backend/category/service/CategoryService.java
+++ src/main/java/com/hcmus/ecommerce_backend/category/service/CategoryService.java
@@ -4,6 +4,8 @@ import com.hcmus.ecommerce_backend.category.model.dto.request.CreateCategoryRequ
 import com.hcmus.ecommerce_backend.category.model.dto.request.UpdateCategoryRequest;
 import com.hcmus.ecommerce_backend.category.model.dto.response.CategoryResponse;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -18,4 +20,6 @@ public interface CategoryService {
     CategoryResponse updateCategory(String id, UpdateCategoryRequest request);
     
     void deleteCategory(String id);
+
+    List<CategoryResponse> getAllCategoriesWithoutPagination();
 }
